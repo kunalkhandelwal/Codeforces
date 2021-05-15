024db36a953570849b62d5adfa6650e88ab40e51
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-16 02:12:47 +0530

    Add files via upload

---
+++ P6 50A Domino Piling.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 22:57:25 2021
+
+@author: kkhan
+"""
+
+a=list(map(int, input().split()))
+m = a[0]
+n = a[1]
+
+area= m*n
+if area%2==0:
+    print(int(area/2))
+else:
+    print(area//2)
+
