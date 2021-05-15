befded54995dd88b9c3faa6db5d119d66c299b8a
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-15 22:52:44 +0530

    Add files via upload

---
+++ P5 158A Next Round.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 21:33:47 2021
+
+@author: kkhan
+"""
+
+b = list(map(int, input().split()))
+n=b[0]
+k=b[1]
+    
+a= list(map(int, input().split()))
+#print(a)
+cnt=0
+for i in range(len(a)):
+    if a[i]>0:
+        if a[i]>=a[k-1]:
+            cnt+=1
+    
+print(cnt)
+    
+    
+
