ed991fb86ff384c35565e2f30f342f9ebc4750fb
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-16 03:42:34 +0530

    Add files via upload

---
+++ P7 118A String Task.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 16 02:18:59 2021
+
+@author: Kunal
+"""
+
+
+"""
+#The below code Didn't work
+#n=str(input(""))
+#n1=n.lower()
+n1=n.lower()
+
+def fun(n1):
+    vowels= ("a","e","i","o","u","y")
+    for x in n1:
+        if x in vowels:
+            n1=n1.replace(x,"")
+        else:
+            n1=n1.replace(x,"."+x)
+        print(n1)
+    
+fun(n1)
+ """       
+n=str(input(""))
+n1=n.lower()  
+s=""
+vowels= ("a","e","i","o","u","y")
+for i in range(0, len(n1)):
+    if n1[i] not in vowels:
+        s=s+"."
+        s=s+n1[i]
+print(s)
+
+         
