2e30110b20e4bd1c1cccd8e760de4bcc6052daea
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-20 02:26:28 +0530

    Add files via upload

---
+++ P17 546A Soldier and Bananas.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 20 01:55:52 2021
+
+@author: kkhan
+"""
+
+k,n,w =map(int, input().split())
+#print(k,n,w)
+count=k1=0
+for i in range(1,w+1):
+        k1=k*i
+        count=count+k1
+if count>n:
+    print(count-n)
+else:
+    print(0)
+    
