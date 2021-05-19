2f44669ba237effadd8053c96d8b3709e0544b11
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-19 17:28:01 +0530

    Add files via upload

---
+++ P15 96A Football.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 19 17:23:05 2021
+
+@author: kkhan
+"""
+n=str(input(""))
+if '0000000' in n:
+    print("YES")
+elif '1111111' in n:
+    print("YES")
+else:
+    print("NO")
