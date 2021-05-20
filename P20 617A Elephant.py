ad86b3e5c0958651606c73bfb1bf15f6847a52b6
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-21 02:36:20 +0530

    Add files via upload

---
+++ P20 617A Elephant.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 20 03:25:34 2021
+
+@author: kkhan
+"""
+
+x=int(input(""))
+count=0
+while x>=5:
+        count=x//5
+        x=x%5
+        #print(count)
+if x%4==0:
+    count=count + x//4
+    #print(count)
+    x=x%4
+elif x%3==0:
+    count= count + x//3
+    #print(count)
+    x=x%3
+elif x%2==0:
+    count=count + x//2
+    #print(count)
+    x=x%2
+else:
+    count=count + x//1
+    #print(count)
+print(count) 
+
+
+"""
+DISCARDED CODE
+
+This was my earlier approach to solve the problem
+x=int(input(""))
+count=0
+if x%5!=0:
+    count=count + x//5
+    x=x%5
+elif x%4!=0:
+    count=count + x//4
+    x=x%4
+elif x%3!=0:
+    count= count + x//3
+    x=x%3
+elif x%2!=0:
+    count=count + x//2
+    x=x%2
+else:
+    count=count + x//1
+print(count)
+"""
