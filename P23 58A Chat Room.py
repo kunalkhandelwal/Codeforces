2add4597d69611ae61c38b81d3bd755518bf0750
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-22 18:38:55 +0530

    Add files via upload

---
+++ P23 58A Chat Room.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 21 23:25:29 2021
+
+@author: kkhan
+"""
+s1='hello'
+def chatRoom():
+    s=str(input())
+#print(type(s))=stringof a string.
+#Whenever we take input without mentioning the type of input, it is always in thr form 
+#print(s)
+    
+    x=0
+    for i in range(len(s)):
+        if s[i]==s1[x]:
+            x=x+1
+        if x==5:
+            return "YES"
+    return"NO"
+print(chatRoom())
+
+
+'''
+My initial approach of solving this question was this, which is not correct.
+for i in range(len(s)):
+    if 'h' in s:
+        count=count+1
+        #print(count)
+        if 'e' in s:
+            count=count+1
+            #print(count)
+            if 'l' in s:
+                count=count+1
+                #print(count)
+                if 'l' in s:
+                    count=count+1
+                    #print(count)
+                    if 'o' in s:
+                        count=count+1
+                        print("YES")
+                        break
+                    else:
+                        print("NO")
+                        break
+
+if count==5:
+    print("YES")
+else:
+    print("NO")                    
+'''
+    
