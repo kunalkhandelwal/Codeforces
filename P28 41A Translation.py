64dfecee344ed8dbf4525e00542cebd764d52811
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-26 01:55:04 +0530

    Add files via upload

---
+++ P28 41A Translation.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 26 01:25:58 2021
+
+@author: kkhan
+"""
+
+s=str(input(""))
+t=str(input(""))
+i=k=0
+#t.reverse() STRING CANNOT BE REVERSED HENCE THIS IS NOT DONE if input is in STRING FORM
+def reverse():
+    string=''
+    for i in s:
+        string=i+string
+    return string
+#print(reverse())
+
+if t==reverse():
+    print("YES")
+else:
+    print("NO")
