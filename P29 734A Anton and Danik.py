75799dacdda245c6eef5b1443b928c95472f9d11
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-26 02:33:20 +0530

    Add files via upload

---
+++ P29 734A Anton and Danik.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 26 02:00:27 2021
+
+@author: kkhan
+"""
+
+n=int(input(""))
+s=str(input(""))
+
+cnt1=cnt2=0
+for i in s:
+    if 'D' in i:
+        cnt1=cnt1+1
+    if 'A' in i:
+        cnt2+=1
+#print(cnt1, cnt2)
+if cnt1>cnt2:
+    print("Danik")
+elif cnt2>cnt1:
+    print("Anton")
+else:
+    print("Friendship")
