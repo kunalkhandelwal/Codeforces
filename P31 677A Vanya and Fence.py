7707f6b78ed7af3a4c67867658c260ded29999d7
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-27 10:11:09 +0530

    Add files via upload

---
+++ P31 677A Vanya and Fence.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 27 10:02:23 2021
+
+@author: kkhan
+"""
+
+n,h=list(map(int, input().split()))
+arr=list(map(int, input().split()))
+#print(n,h,arr)
+cnt=0
+for i in range(0,n):
+    if arr[i]>h:
+        cnt=cnt+2
+    else:
+        cnt=cnt+1
+print(cnt)
