2a7e7540c0231a1833c0dbfdc074a72aaff14880
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-28 00:05:44 +0530

    Add files via upload

---
+++ P32 133A HQ9+.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 27 10:11:19 2021
+
+@author: kkhan
+"""
+
+p=str(input(""))
+#print(len(p))
+while(p):
+    if 'H' in p:
+        print("YES")
+        break
+    elif 'Q' in p:
+        print("YES")
+        break
+    elif '9' in p:
+        print("YES")
+        break
+#    elif '+' in p:
+#        print("YES")
+#        break
+    else:
+        print("NO")
+        break    
+    
+    
+'''
+I DON'T KNOW WHY WAS THIS CODE GIVING ERROR ON TESTCASE 10: 
+    dEHsbM'gS[\brZ_dpjXw8f?L[4E"s4Zc9*(,j:>p$}m7HD[_9nOWQ\uvq2mHWR
+for i in range(len(p)):
+#while(len(p)):
+    if 'H' in p[i]:
+        print("YES")
+        break
+    elif 'Q' in p[i]:
+        print("YES")
+        break
+    elif '9' in p[i]:
+        print("YES")
+        break
+    elif '+' in p[i]:
+        print("YES")
+        break
+    else:
+        print("NO")
+        break
+'''
