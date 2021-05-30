ec369e9f5afbf27be18588a2acf066b0e9c3cc31
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-30 06:40:48 +0530

    Add files via upload

---
+++ P35 1080A In Search of an Easy Problem.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 29 18:15:23 2021
+
+@author: kkhan
+"""
+
+n=int(input())
+a=list(map(int, input().split()))
+#print(a)
+cnt1=0
+for i in range(n):
+    if 1 in a:
+        cnt1=cnt1+1
+        #print(cnt1)
+if cnt1>0:
+    print("HARD")
+else:
+    print("EASY")
