905be78eb1cdd5001cd87e6f0d23cc62b992d1d8
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-05-30 06:32:16 +0530

    Add files via upload

---
+++ P36 344A Magnets.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 29 22:40:59 2021
+
+@author: kkhan
+"""
+n=int(input())
+#arr=list(map(int, input().split()))
+#print(arr)
+a=[]
+count=0
+for i in range(n):
+    a.append(str(input()))
+#print(a)
+for i in range(1,n):
+    if a[i-1]!=a[i]:
+        count=count+1
+print(count+1) 
