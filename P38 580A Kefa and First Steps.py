81d2c957c4f581c66274056194c7e7132dfa7143
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-06-01 11:30:17 +0530

    Add files via upload

---
+++ P38 580A Kefa and First Steps.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 31 02:09:24 2021
+
+@author: kkhan
+"""
+#https://codeforces.com/problemset/problem/580/A
+
+n=int(input())
+a=list(map(int, input().split()))
+cnt1=1
+ans=1
+l=[]
+for i in range(1,n):
+    if a[i-1]<=a[i]:
+        cnt1=cnt1+1
+        l.append(i)
+    else:
+        cnt1=1
+    ans=max(ans,cnt1)
+        #print(i)
+print(ans)
+    
