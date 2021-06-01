6f5034660a88c57b9b9647c74419a021f1c4b3be
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-06-01 13:03:06 +0530

    Add files via upload

---
+++ P41 61A Ultra-Fast Mathematician.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  1 12:56:12 2021
+
+@author: kkhan
+"""
+n1=str(input())
+n2=str(input())
+s=''
+#print(n1,n2)
+
+for i in range(len(n1)):
+    if n1[i]!=n2[i]:
+        s=s+'1'
+    else:
+        s=s+'0'
+print(s)
+        
