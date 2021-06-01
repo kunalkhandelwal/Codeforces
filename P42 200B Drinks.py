426000fceb351daae13de1a7e2a7f98ef25fad29
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-06-01 13:12:20 +0530

    Add files via upload

---
+++ P42 200B Drinks.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  1 13:04:10 2021
+
+@author: kkhan
+"""
+
+n=int(input())
+a=list(map(int, input().split()))
+vol=0
+x=1
+total=n*x
+for i in range(len(a)):
+    k=(a[i]/100)*x
+    vol=vol+k
+print((vol/total)*100)
+    
