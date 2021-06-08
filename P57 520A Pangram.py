da0635971d29165f39544a50f9465caec13e17dd
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2021-06-08 07:31:14 +0530

    Update P57 520A Pangram.py

--- P57 520A Pangram.py
+++ P57 520A Pangram.py
@@ -16,12 +16,7 @@ for i in range(n):
     if ord(a1[i])>=97 and ord(a1[i])<=122:
         s.add(ord(a1[i]))
 #print(s)
-'''
-if cnt>=26:
-    print("YES")
-else:
-    print("NO")
- '''
+
 if len(s)==26:
     print("YES")
 else:
